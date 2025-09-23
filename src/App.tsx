a377df015cc7c27b8a58642a94a41d66ec8bdc4c
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2025-09-23 12:44:08 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import {  
+  Route,
+  Routes
+  } 
+from 'react-router-dom';
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+  <>
+      {/* <Header /> */}
+      <Routes>
+        {/*
+        <Route path='/materials' element={<MaterialsPage />} />
+        <Route path='/blog' element={<BlogPage />}></Route>
+        <Route path='/blog/:id' element={<BlogArticlePage />} />
+        <Route path='/catalog' element={<CatalogPage />}>
+          <Route index element={<Wallpapers />} />
+          <Route path=':cid' element={<Category />} />
+        </Route>
+        <Route path='*' element={<MainPage />} /> */}
+      </Routes>
+      {/* <Footer /> */}
+    </>
+  )
+}
+
+export default App
